1f8410d0a0a7019e12479e7694eb6b362dbf7f7f
Saurabh Singh <ss21xtar@gmail.com>
2024-10-22 19:57:43 +0530

    Added a route for creating and storing rules in DB

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from sqlalchemy import create_engine, Column, Integer, String, Text
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+import json
+import logging
 
 app = Flask(__name__)
 
-@app.route('/')
+Base = declarative_base()
+class RuleModeL(Base):
+    __tablename__ = 'rules'
+    id = Column(Integer, primary_key=True)
+    rule_expression = Column(String, nullable=False)
+    rule_ast = Column(Text, nullable=False)
+    
+#SQLLite database Initialization
+engine = create_engine('sqlite:///rule.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
-#Testing the flask app for the first time
-def index():
-    return "Testing flask app"
-
-
+@app.route('/create_rule', methods=['POST'])
+def create_rule():
+    rule_expression = request.json.get('rule_expression')
+    rule_ast = json.dumps({'example': 'AST'})
+    new_rule = RuleModeL(rule_expression=rule_expression, rule_ast=rule_ast)
+    session.add(new_rule)
+    session.commit()
+    return jsonify({'id': new_rule.id, 'rule_ast': new_rule.rule_ast})
 if __name__ == '__main__':
     app.run(debug=True)
